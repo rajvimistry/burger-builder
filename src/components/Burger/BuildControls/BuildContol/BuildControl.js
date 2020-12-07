818c641cd63767f4152846a4cc5b3a7091140f84
Rajvi Mistry <rajvimistry@Rajvis-MBP.lan>
2020-12-07 12:59:47 -0600

    Adding Order Now button and Displaying total amount

--- src/components/Burger/BuildControls/BuildContol/BuildControl.js
+++ src/components/Burger/BuildControls/BuildContol/BuildControl.js
@@ -3,9 +3,11 @@ import classes from './BuildControl.css'
 
 const buildControl = (props) => (
     <div className={classes.BuildControl}>
-        <div classes={classes.Label}>{props.label}</div>
-        <button classes={classes.Less}>Less</button>
-        <button classes={classes.More}>More</button>
+        <div className={classes.Label}>{props.label}</div>
+        <button className={classes.Less} 
+                onClick={props.removed} 
+                disabled={props.disabled}>Less</button>
+        <button className={classes.More} onClick={props.added}>More</button>
     </div>
 );
 
